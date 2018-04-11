ace93d093edc8004289a7b95f069fb5cdeb0d2a2
leohughes <zhihy123@qq.com>
2018-04-11 16:29:11 +0800

    Update main page

--- src/store/modules/MenuModule.js
+++ src/store/modules/MenuModule.js
@@ -1,15 +1,23 @@
 import axios from '@/util/instance'
 import config from '@/config'
-
+import _ from 'lodash'
 import { Message } from 'iview'
 
 export default {
   state: {
     mainLeftMenu: null, //主页面左侧菜单数据
+    openTabs: [], //激活的左侧菜单页
+    activeTab: null, //当前查看的左侧菜单页
   },
   getters: {
     mainLeftMenu(state) {
       return state.mainLeftMenu
+    },
+    openTabs(state) {
+      return state.openTabs
+    },
+    activeTab(state) {
+      return state.activeTab
     }
   },
   mutations: {
@@ -18,6 +26,21 @@ export default {
     },
     clearMainLeftMenuData(state) {
       state.mainLeftMenu = null
+    },
+    //更新激活的左侧菜单页数据 type: 0新增1删除2删除全部
+    updateOpenTabs(state, { type, tabObj }) {
+
+      switch (type) {
+        case 0:
+          if (_.findIndex(state.openTabs, tabObj) == -1) {
+            state.openTabs.push(tabObj)
+          }
+          break
+      }
+    },
+    //更新当前查看的左侧菜单页数据
+    updateActiveTab(state, payload) {
+      state.activeTab = payload
     }
   },
   actions: {
--- src/view/Index.vue
+++ src/view/Index.vue
@@ -12,14 +12,14 @@
       <div class="layout-logo"></div>
       <!-- logo end-->
 
-      <Menu :active-name="$route.name" theme="dark" width="auto" :class="isCollapsed ? 'miniMenu':''"  @on-open-change="resetSider">
-        <Submenu v-for="(item,index) in mainLeftMenu" :key="index" :name="item.name">
+      <Menu ref="leftMenu" accordion :open-names="[activeName]" :active-name="$route.name" theme="dark" width="auto" :class="isCollapsed ? 'miniMenu':''" @on-open-change="resetSider">
+        <Submenu v-for="(item,i) in mainLeftMenu" :key="i" :name="item.name">
           <template slot="title">
             <Icon :type="item.icon"></Icon>
             <span>{{ item.title }}</span>
           </template>
-          <router-link v-show="!isCollapsed" v-for="(child,index) in item.children" :key="index" :to="{path: child.path }">
-            <MenuItem :name="child.name">{{ child.title }}</MenuItem>
+          <router-link v-show="!isCollapsed" v-for="(child,i) in item.children" :key="i" :to="{path: child.path }">
+            <MenuItem :name="child.name" @click.native="addTabs(child)">{{ child.title }}</MenuItem>
           </router-link>
         </Submenu>
       </Menu>
@@ -50,6 +50,14 @@
 
       <!-- content -->
       <Content :style="{padding:'10px'}">
+        <Row>
+          <Col span="24">
+          <router-link v-for="(item,i) in openTabs" :key="i" :to="{path:item.path}">
+            <Tag type="dot" closable color="blue" @click.native="updateTab(item)">{{ item.title }}</Tag>
+          </router-link>
+          </Col>
+        </Row>
+
         <Card>
           <div>
             <router-view></router-view>
@@ -63,34 +71,59 @@
 </template>
 
 <script>
-import '@/style/Index.less'
-import { mapGetters, mapActions } from "vuex";
+import "@/style/Index.less";
+import { mapGetters, mapMutations, mapActions } from "vuex";
 
 export default {
   name: "Index",
   data() {
     return {
-      isCollapsed: false, //是否收起边栏
+      isCollapsed: false //是否收起边栏
     };
   },
   computed: {
-    ...mapGetters(["token", "userInfo", "mainLeftMenu"]),
-    mainCss(){
-      return [
-        'main',
-        this.isCollapsed ? 'main-collapse' : ''
-      ]
+    ...mapGetters([
+      "token",
+      "userInfo",
+      "mainLeftMenu",
+      "openTabs",
+      "activeTab"
+    ]),
+    mainCss() {
+      return ["main", this.isCollapsed ? "main-collapse" : ""];
+    },
+    activeName() {
+      return this.activeTab !== null ? this.activeTab.parentName : "";
     }
   },
   beforeMount() {
     this.$Spin.show();
+    //获取左侧菜单
     this.getMainLeftMenu(this.token);
   },
   updated() {
-    this.$Spin.hide();
+    //默认展开左侧第一个菜单项第一个子页面
+    if (this.mainLeftMenu && this.mainLeftMenu.length !== 0) {
+      this.addTabs(this.mainLeftMenu[0].children[0]);
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.$Spin.hide();
+    }, 1000);
+  },
+  watch: {
+    activeName() {
+      //左侧菜单展开
+      this.$nextTick(() => {
+        this.$refs.leftMenu.updateOpened();
+        this.$refs.leftMenu.updateActiveName();
+      });
+    }
   },
   methods: {
     ...mapActions(["getMainLeftMenu", "loginOut"]),
+    ...mapMutations(["updateOpenTabs", "updateActiveTab"]),
     //登出
     userLoginOut() {
       this.loginOut();
@@ -98,14 +131,23 @@ export default {
     },
     //收起边栏
     toggleSider() {
-        this.$refs.sider.toggleCollapse();
-        this.isCollapsed = !this.isCollapsed;
+      this.$refs.sider.toggleCollapse();
+      this.isCollapsed = !this.isCollapsed;
     },
     //边栏收起状态下点击菜单图标则展开
-    resetSider(){
-      if(this.isCollapsed){
+    resetSider() {
+      if (this.isCollapsed) {
         this.toggleSider();
       }
+    },
+    //增加激活左侧子菜单tab数据
+    addTabs(tabObj) {
+      this.updateOpenTabs({ type: 0, tabObj });
+      this.updateTab(tabObj);
+    },
+    //更新当前激活tab数据
+    updateTab(activeObj) {
+      this.updateActiveTab(activeObj);
     }
   }
 };
