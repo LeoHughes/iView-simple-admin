db33860bb34c6d873b32b641e72facd1e7332cc4
leohughes <zhihy123@qq.com>
2018-04-12 14:28:56 +0800

    Update main page and login

--- src/view/Index.vue
+++ src/view/Index.vue
@@ -6,58 +6,17 @@
   <Layout class="layout">
 
     <!-- 侧边栏 -->
-    <Sider ref="sider" collapsible hide-trigger :collapsed-width="80" breakpoint="md" :style="{position: 'fixed', height: '100vh', left: 0, overflow: 'auto'}">
-
-      <!-- logo -->
-      <div class="layout-logo"></div>
-      <!-- logo end-->
-
-      <Menu ref="leftMenu" accordion :open-names="[activeName]" :active-name="$route.name" theme="dark" width="auto" :class="isCollapsed ? 'miniMenu':''" @on-open-change="resetSider">
-        <Submenu v-for="(item,i) in mainLeftMenu" :key="i" :name="item.name">
-          <template slot="title">
-            <Icon :type="item.icon"></Icon>
-            <span>{{ item.title }}</span>
-          </template>
-          <router-link v-show="!isCollapsed" v-for="(child,i) in item.children" :key="i" :to="{path: child.path }">
-            <MenuItem :name="child.name" @click.native="addTabs(child)">{{ child.title }}</MenuItem>
-          </router-link>
-        </Submenu>
-      </Menu>
-    </Sider>
+    <SiderMenu ref="siderMenu" :isSiderCollapsed="isSiderCollapsed" @changeSiderStatus="changeSiderCollapsed">
+    </SiderMenu>
     <!-- 侧边栏 end-->
 
     <Layout :class="mainCss">
       <!-- 头部 -->
-      <Header class="layout-header-bar">
-        <div class="collapsible-icon">
-          <Icon :type="isCollapsed ? 'chevron-left' : 'chevron-right'" size="16" @click.native="toggleSider"></Icon>
-        </div>
-
-        <Dropdown class="header-control">
-          <a href="javascript:void(0)">
-            {{ userInfo.roleName }}
-            <Icon type="arrow-down-b"></Icon>
-          </a>
-          <DropdownMenu slot="list">
-            <DropdownItem>个人中心</DropdownItem>
-            <DropdownItem>
-              <p @click="userLoginOut">退出</p>
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      </Header>
+      <MainHeader :isSiderCollapsed="isSiderCollapsed" @toggleSiderMenu="toggleSiderMenu"></MainHeader>
       <!-- 头部 end-->
 
       <!-- content -->
       <Content :style="{padding:'10px'}">
-        <Row>
-          <Col span="24">
-          <router-link v-for="(item,i) in openTabs" :key="i" :to="{path:item.path}">
-            <Tag type="dot" closable color="blue" @click.native="updateTab(item)">{{ item.title }}</Tag>
-          </router-link>
-          </Col>
-        </Row>
-
         <Card>
           <div>
             <router-view></router-view>
@@ -74,80 +33,43 @@
 import "@/style/Index.less";
 import { mapGetters, mapMutations, mapActions } from "vuex";
 
+import SiderMenu from "@/components/main/SiderMenu";
+import MainHeader from '@/components/main/Header';
+
 export default {
   name: "Index",
+  components: {
+    SiderMenu,
+    MainHeader
+  },
   data() {
     return {
-      isCollapsed: false //是否收起边栏
+      isSiderCollapsed: false //是否收起边栏
     };
   },
   computed: {
-    ...mapGetters([
-      "token",
-      "userInfo",
-      "mainLeftMenu",
-      "openTabs",
-      "activeTab"
-    ]),
+    ...mapGetters(["token", "userInfo"]),
     mainCss() {
-      return ["main", this.isCollapsed ? "main-collapse" : ""];
-    },
-    activeName() {
-      return this.activeTab !== null ? this.activeTab.parentName : "";
+      return ["main", this.isSiderCollapsed ? "main-collapse" : ""];
     }
   },
   beforeMount() {
     this.$Spin.show();
-    //获取左侧菜单
-    this.getMainLeftMenu(this.token);
-  },
-  updated() {
-    //默认展开左侧第一个菜单项第一个子页面
-    if (this.mainLeftMenu && this.mainLeftMenu.length !== 0) {
-      this.addTabs(this.mainLeftMenu[0].children[0]);
-    }
   },
   mounted() {
     setTimeout(() => {
       this.$Spin.hide();
     }, 1000);
   },
-  watch: {
-    activeName() {
-      //左侧菜单展开
-      this.$nextTick(() => {
-        this.$refs.leftMenu.updateOpened();
-        this.$refs.leftMenu.updateActiveName();
-      });
-    }
-  },
   methods: {
-    ...mapActions(["getMainLeftMenu", "loginOut"]),
-    ...mapMutations(["updateOpenTabs", "updateActiveTab"]),
-    //登出
-    userLoginOut() {
-      this.loginOut();
-      this.$router.push({ path: "/login" });
-    },
+    ...mapActions(["loginOut"]),
     //收起边栏
-    toggleSider() {
-      this.$refs.sider.toggleCollapse();
-      this.isCollapsed = !this.isCollapsed;
+    toggleSiderMenu() {
+      this.changeSiderCollapsed();
+      this.$refs.siderMenu.toggleSider();
     },
-    //边栏收起状态下点击菜单图标则展开
-    resetSider() {
-      if (this.isCollapsed) {
-        this.toggleSider();
-      }
-    },
-    //增加激活左侧子菜单tab数据
-    addTabs(tabObj) {
-      this.updateOpenTabs({ type: 0, tabObj });
-      this.updateTab(tabObj);
-    },
-    //更新当前激活tab数据
-    updateTab(activeObj) {
-      this.updateActiveTab(activeObj);
+    changeSiderCollapsed() {
+      this.isSiderCollapsed = !this.isSiderCollapsed;
     }
   }
 };
--- src/view/Login.vue
+++ src/view/Login.vue
@@ -1,28 +1,28 @@
 <template>
   <Row type="flex" justify="center" class="code-row-bg">
     <i-col :xs="22" :sm="18" :md="12" :lg="8">
-     
-     <section class="formContent">
-         <h1 class="title">Login in Management</h1>
+
+      <section class="formContent">
+        <h1 class="title">Login in Management</h1>
         <Form class="formBody" ref="formData" :model="formData" label-position="left" :rules="validate">
-              <FormItem label="Account" prop="userName"> 
-                <Row>
-                  <Col span="24">
-                    <Input v-model="formData.userName" type="text" size="large" placeholder="please enter account" :maxlength=10></Input>
-                  </Col>
-                </Row>
-              </FormItem>
-              <FormItem label="password" prop="passWord">
-                <Row>
-                  <Col span="24">
-                    <Input v-model="formData.passWord" type="password" size="large" placeholder="please enter password" :maxlength=16></Input>
-                  </Col>
-                </Row>
-              </FormItem>
-              <Button type="primary" @click="formSumbit('formData')">Submit</Button>
-              <Button type="ghost" @click="formRest" style="margin-left: 8px">Reset</Button>
-      </Form>
-    </section>
+          <FormItem label="Account" prop="userName">
+            <Row>
+              <Col span="24">
+              <Input v-model="formData.userName" type="text" size="large" placeholder="please enter account" :maxlength=10></Input>
+              </Col>
+            </Row>
+          </FormItem>
+          <FormItem label="password" prop="passWord">
+            <Row>
+              <Col span="24">
+              <Input v-model="formData.passWord" type="password" size="large" placeholder="please enter password" :maxlength=16></Input>
+              </Col>
+            </Row>
+          </FormItem>
+          <Button type="primary" :loading="loading" @click="formSumbit('formData')">Submit</Button>
+          <Button type="ghost" @click="formRest" style="margin-left: 8px">Reset</Button>
+        </Form>
+      </section>
     </i-col>
   </Row>
 </template>
@@ -31,27 +31,25 @@
 .formContent {
   margin-top: 160px;
   text-align: center;
-  .formBody{
-    background-color:#fff;
+  .formBody {
+    background-color: #fff;
     display: inline-block;
     width: 340px;
-    padding:25px 30px;
+    padding: 25px 30px;
     border: 1px solid #d8dee2;
     border-radius: 4px;
-    margin-top:35px;
+    margin-top: 35px;
   }
 
-  .title{
+  .title {
     width: 100%;
     font-size: 24px;
   }
 }
-
-
 </style>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapGetters, mapActions } from "vuex";
 
 export default {
   name: "Login",
@@ -66,31 +64,31 @@ export default {
       validate: {
         userName: [{ required: true, min: 6, message: "请输入用户名!" }],
         passWord: [{ required: true, min: 6, message: "请输入密码!" }]
-      }
+      },
+      //登录按钮等待(放重复提交)
+      loading: false
     };
   },
+  computed: {
+    ...mapGetters(["userInfo"])
+  },
   methods: {
-    ...mapActions(['login']),
+    ...mapActions(["login"]),
     formSumbit(name) {
-      this.$refs[name].validate(valid => {
+      this.$refs[name].validate(async valid => {
         if (valid) {
+          this.loading = !this.loading;
 
-          // this.$store.dispatch("login", {
-          //     userName: this.formData.userName,
-          //     passWord: this.formData.passWord
-          //   })
-          //   .then(() => {
-          //     this.$router.push({ path: "/" });
-          //   });
+          await this.login({
+            userName: this.formData.userName,
+            passWord: this.formData.passWord
+          });
 
-          this.login({
-              userName: this.formData.userName,
-              passWord: this.formData.passWord
-            })
-            .then(() => {
-              this.$router.push({ path: "/" });
-            });
-
+          if (!this.userInfo.error) {
+            this.$router.push({ path: "/" });
+          } else {
+            this.$Message.error(this.userInfo.error.message);
+          }
         }
       });
     },
