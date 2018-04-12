ccee5309b568c454d3eee27be94fce8075d1157e
leohughes <zhihy123@qq.com>
2018-04-12 17:55:23 +0800

    Add view file

---
+++ src/view/UserCenter/Center.vue
@@ -0,0 +1,19 @@
+<template>
+  <h3>UserCenter</h3>
+</template>
+
+<script>
+export default {
+  name: "UserCenter",
+  data() {
+    return {
+      
+    };
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+
+</style>
---
+++ src/view/UserCenter/Message.vue
@@ -0,0 +1,19 @@
+<template>
+  <h3>Message</h3>
+</template>
+
+<script>
+export default {
+  name: "Message",
+  data() {
+    return {
+      
+    };
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+
+</style>
