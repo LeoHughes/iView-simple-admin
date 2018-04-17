b4c93d09b9a6b787ca57b8ade2bc7338d572c3c5
leohughes <zhihy123@qq.com>
2018-04-17 10:50:29 +0800

    Update router config

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,53 +2,78 @@
 export default [{
     path: "*",
     name: "404",
+    meta: {
+      title: "error-404"
+    },
     component: () =>
       import ("@/view/NotFound")
   },
   {
     path: "/403",
     name: "403",
+    meta: {
+      title: "error-403"
+    },
     component: () =>
       import ("@/view/NotAuthorized")
   },
   {
     path: "/login",
     name: "Login",
+    meta: {
+      title: "登录页"
+    },
     component: () =>
       import ("@/view/Login")
   },
   {
     path: "/",
     name: "Index",
-    meta: { requireAuth: true },
+    meta: {
+      requireAuth: true,
+      title: "主页"
+    },
     component: () =>
       import ("@/view/Index"),
     children: [{
-        path: "/test",
+        path: "/test/1",
         name: "Test",
-        meta: { requireAuth: true },
+        meta: {
+          requireAuth: true,
+          title: "测试子菜单1"
+        },
         component: () =>
           import ("@/view/Test/Test")
-      }, {
-        path: '/test/Child',
-        name: 'TestChild',
-        meta: { requireAuth: true },
-        component: () =>
-          import ('@/view/Test/TestChild')
       },
       {
-        path: '/message',
-        name: 'Message',
-        meta: { requireAuth: true },
+        path: "/test/2",
+        name: "TestChild",
+        meta: {
+          requireAuth: true,
+          title: "测试子菜单2"
+        },
         component: () =>
-          import ('@/view/UserCenter/Message')
+          import ("@/view/Test/TestChild")
       },
       {
-        path: '/user/center',
-        name: 'UserCenter',
-        meta: { requireAuth: true },
+        path: "/message",
+        name: "Message",
+        meta: {
+          requireAuth: true,
+          title: "消息中心"
+        },
         component: () =>
-          import ('@/view/UserCenter/Center')
+          import ("@/view/UserCenter/Message")
+      },
+      {
+        path: "/user/center",
+        name: "UserCenter",
+        meta: {
+          requireAuth: true,
+          title: "个人中心"
+        },
+        component: () =>
+          import ("@/view/UserCenter/Center")
       }
     ]
   }
