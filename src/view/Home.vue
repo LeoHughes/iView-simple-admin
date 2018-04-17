d95d1d04e6f04433f7c1461c70689dcf0c82c0d0
leohughes <zhihy123@qq.com>
2018-04-17 11:41:21 +0800

    Add home view file

---
+++ src/view/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <h3>Home</h3>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      
+    };
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+
+</style>
